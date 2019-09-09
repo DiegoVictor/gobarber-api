ac2ea603a064a34fe100ebde0fcbc458a719cd93
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-09-09 12:13:34 -0300

    Retrieve user avatar and provider flag

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 import { SECRET, EXPIRATION_TIME } from '../../config/auth';
 
@@ -19,7 +20,16 @@ class SessionController {
 
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
     if (!user) {
       return res.status(401).json({
         error: 'User not found',
@@ -32,9 +42,9 @@ class SessionController {
       });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
     return res.json({
-      user: { id, name, email },
+      user: { id, name, email, avatar, provider },
       token: jwt.sign({ id }, SECRET, {
         expiresIn: EXPIRATION_TIME,
       }),
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -69,8 +70,16 @@ class UserController {
       });
     }
 
-    const { id, name, provider } = await user.update(req.body);
-    return res.json({ id, name, email, provider });
+    await user.update(req.body);
+
+    const { id, name, avatar } = await User.findByPk(req.user_id, {
+      include: {
+        model: File,
+        as: 'avatar',
+        attributes: ['id', 'url', 'path'],
+      },
+    });
+    return res.json({ id, name, email, avatar });
   }
 }
 
