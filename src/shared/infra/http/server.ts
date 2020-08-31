eafea8a55964343838ead475f52c6cf1d021d5e7
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-31 07:32:50 -0300

    Install cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 const app = express();
 
+app.use(cors());
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
 });
