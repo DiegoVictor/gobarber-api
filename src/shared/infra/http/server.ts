b5f216c64d21219d42dfbfd5f4bdf205d156d4bb
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-02 19:00:37 -0300

    Create error handler

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,12 +3,28 @@ import cors from 'cors';
 import helmet from 'helmet';
 import { errors } from 'celebrate';
 
+import AppError from '@shared/errors/AppError';
+
 const app = express();
 
 app.use(cors());
 app.use(helmet());
 
 app.use(errors());
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
 });
