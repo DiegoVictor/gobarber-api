47bfdbecfce3ad2a62f223ddec0421068b8c2155
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-02 18:58:05 -0300

    Handle errors with celebrate

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import { errors } from 'celebrate';
+
 const app = express();
 
 app.use(cors());
 app.use(helmet());
+
+app.use(errors());
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
 });
