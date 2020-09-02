9c44ab091bb13c6c36f6539de1a2fb10f9c2e83f
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-02 18:54:18 -0300

    Configure helmet

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 const app = express();
 
 app.use(cors());
+app.use(helmet());
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
 });
