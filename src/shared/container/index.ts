b7fbc4c62ae16f8668ff3a12ca4a0b6f1b53af5d
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-03 20:45:27 -0300

    Allow dependency injection of users repository

---
+++ src/shared/container/index.ts
@@ -0,0 +1,8 @@
+import { container } from 'tsyringe';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
+);
