8bb9945ec2b8220933f60b8c56ddd9d3f1c4f617
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-04 19:39:24 -0300

    Create authentication service

---
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -0,0 +1,53 @@
+import { sign } from 'jsonwebtoken';
+import { injectable, inject } from 'tsyringe';
+
+import User from '../infra/typeorm/entities/User';
+import authConfig from '@config/auth';
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: User;
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+    if (!user) {
+      throw new AppError('Email and/or password invalid', 401);
+    }
+
+    const passwordMatched = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
+    if (!passwordMatched) {
+      throw new AppError('Email and/or password invalid', 401);
+    }
+
+    const token = sign({}, authConfig.jwt.secret, {
+      subject: user.id,
+      expiresIn: authConfig.jwt.expiresIn,
+    });
+
+    return { user, token };
+  }
+}
+
+export default AuthenticateUserService;
