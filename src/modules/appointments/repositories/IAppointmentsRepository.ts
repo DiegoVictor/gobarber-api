a105db6f4b6e8dd2d447248f30af8544735b59d9
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-11-01 12:33:29 -0300

    Create appointment repository interface

---
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -0,0 +1,15 @@
+import Appointment from '../infra/typeorm/entities/Appointment';
+import ICreateAppoitmentDTO from '../dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '../dtos/IFindAllInDayFromProviderDTO';
+
+export default interface IAppointmentsRepository {
+  create(data: ICreateAppoitmentDTO): Promise<Appointment>;
+  findByDate(date: Date, provider_id: string): Promise<Appointment | undefined>;
+  findAllInMonthFromProvider(
+    data: IFindAllInMonthFromProviderDTO,
+  ): Promise<Appointment[]>;
+  findAllInDayFromProvider(
+    data: IFindAllInDayFromProviderDTO,
+  ): Promise<Appointment[]>;
+}
