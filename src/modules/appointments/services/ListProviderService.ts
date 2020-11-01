b03e7808197e67f7a27a81a861fc255afa335126
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-11-01 12:29:01 -0300

    Return providers from cache or database

---
+++ src/modules/appointments/services/ListProviderService.ts
@@ -0,0 +1,42 @@
+import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+
+interface IRequest {
+  user_id: string;
+}
+
+@injectable()
+class ListProviderService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
+  ) {}
+
+  public async execute({ user_id }: IRequest): Promise<User[]> {
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers_list:${user_id}`,
+    );
+
+    if (!users) {
+      users = await this.usersRepository.findAllProviders({
+        except_user_id: user_id,
+      });
+
+      await this.cacheProvider.save(
+        `providers_list:${user_id}`,
+        classToClass(users),
+      );
+    }
+
+    return users;
+  }
+}
+
+export default ListProviderService;
