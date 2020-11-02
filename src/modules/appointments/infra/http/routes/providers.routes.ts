cabd1e9051439b767627e30fd815b21ff74acf06
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-11-02 15:42:46 -0300

    Configure providers routes

---
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import ProvidersController from '../controllers/ProvidersController';
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
+
+const routes = Router();
+const providersController = new ProvidersController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
+
+routes.use(ensureAuthenticated);
+
+routes.get('/', providersController.index);
+routes.get(
+  '/:provider_id/month_availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    },
+  }),
+  providerMonthAvailabilityController.index,
+);
+routes.get(
+  '/:provider_id/day_availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    },
+  }),
+  providerDayAvailabilityController.index,
+);
+
+export default routes;
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 
+import providersRoutes from '@modules/appointments/infra/http/routes/providers.routes';
 import sessionsRoutes from '@modules/users/infra/http/routes/sessions.routes';
 
 const routes = Router();
 
 routes.use('/sessions', sessionsRoutes);
+
+routes.use('/providers', providersRoutes);
 export default routes;
