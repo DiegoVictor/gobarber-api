951f52738be20dae949f1abe9cba77c59c9bca4b
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-11-06 19:12:47 -0300

    Create reset password process routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import providersRoutes from '@modules/appointments/infra/http/routes/providers.routes';
 import sessionsRoutes from '@modules/users/infra/http/routes/sessions.routes';
+import passwordRoutes from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/sessions', sessionsRoutes);
 
 routes.use('/providers', providersRoutes);
+routes.use('/password', passwordRoutes);
 export default routes;
