17a07308e4e9bd69a1ac11e04804401290dad1f5
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-11-07 19:30:55 -0300

    Create a new user

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,48 @@
+import { injectable, inject } from 'tsyringe';
+
+import User from '../infra/typeorm/entities/User';
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+@injectable()
+class CreateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
+  ) {}
+
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const userExists = await this.usersRepository.findByEmail(email);
+    if (userExists) {
+      throw new AppError('Email address already in use');
+    }
+
+    const hashedPassword = await this.hashProvider.generateHash(password);
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await this.cacheProvider.invalidatePrefix('providers_list');
+
+    return user;
+  }
+}
+
+export default CreateUserService;
