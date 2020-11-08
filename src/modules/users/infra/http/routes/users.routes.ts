a00810863af62a1fe0bb620c8611834ec945872d
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-11-08 13:01:39 -0300

    Configure users' routes

---
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+import multer from 'multer';
+import { celebrate, Segments, Joi } from 'celebrate';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+import uploadConfig from '@config/upload';
+import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
+
+const routes = Router();
+const upload = multer(uploadConfig.multer);
+const usersControllers = new UsersController();
+const userAvatarController = new UserAvatarController();
+
+routes.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersControllers.create,
+);
+
+routes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  userAvatarController.update,
+);
+
+export default routes;
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import providersRoutes from '@modules/appointments/infra/http/routes/providers.routes';
+import usersRoutes from '@modules/users/infra/http/routes/users.routes';
 import sessionsRoutes from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRoutes from '@modules/users/infra/http/routes/password.routes';
 
@@ -9,5 +10,6 @@ const routes = Router();
 routes.use('/sessions', sessionsRoutes);
 
 routes.use('/providers', providersRoutes);
+routes.use('/users', usersRoutes);
 routes.use('/password', passwordRoutes);
 export default routes;
