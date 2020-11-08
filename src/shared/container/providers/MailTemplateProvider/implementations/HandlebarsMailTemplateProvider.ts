616b7c78bddedc2bfcdf732111a91342df285e50
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-11-08 13:17:32 -0300

    Create handlebars mail template provider

---
+++ src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
@@ -0,0 +1,17 @@
+import handlebars from 'handlebars';
+import fs from 'fs';
+
+import IMailTemplateProvider from '../models/IMailTemplateProvider';
+import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
+
+class HandlebarsMailTemplateProvider implements IMailTemplateProvider {
+  public async parse({ file, variables }: IParseMailTemplateDTO) {
+    const templateFileContent = await fs.promises.readFile(file, {
+      encoding: 'utf-8',
+    });
+    const parseTemplate = handlebars.compile(templateFileContent);
+    return parseTemplate(variables);
+  }
+}
+
+export default HandlebarsMailTemplateProvider;
