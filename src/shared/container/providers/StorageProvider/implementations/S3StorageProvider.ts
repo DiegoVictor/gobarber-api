7ca3ec584c833f8daa770bf484f1ce947de8d77c
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-11-08 13:15:28 -0300

    Create provider to manage files in aws storage

---
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -0,0 +1,52 @@
+import fs from 'fs';
+import path from 'path';
+import aws, { S3 } from 'aws-sdk';
+import mime from 'mime';
+
+import uploadConfig from '@config/upload';
+import IStorageProvider from '../models/IStorageProvider';
+
+class S3StorageProvider implements IStorageProvider {
+  private client: S3;
+
+  constructor() {
+    this.client = new aws.S3({
+      region: process.env.AWS_DEFAULT_REGION,
+    });
+  }
+
+  public async saveFile(file: string): Promise<string> {
+    const originalPath = path.resolve(uploadConfig.tmpDirectory, file);
+
+    const fileContent = await fs.promises.readFile(originalPath);
+
+    const contentType = mime.getType(originalPath);
+    if (!contentType) {
+      throw new Error('File not found');
+    }
+
+    await this.client
+      .putObject({
+        Bucket: uploadConfig.config.aws.bucket,
+        Key: file,
+        ACL: 'public-read',
+        Body: fileContent,
+        ContentType: contentType,
+      })
+      .promise();
+
+    await fs.promises.unlink(originalPath);
+
+    return file;
+  }
+
+  public async deleteFile(file: string): Promise<void> {
+    await this.client
+      .deleteObject({
+        Bucket: uploadConfig.config.aws.bucket,
+        Key: file,
+      })
+      .promise();
+  }
+}
+export default S3StorageProvider;
