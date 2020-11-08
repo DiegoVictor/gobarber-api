bb2cb55aabd4696a2dea3a76c7ea454dcf587c8a
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-11-08 13:16:09 -0300

    Choose storage provider based on environment

---
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -0,0 +1,17 @@
+import { container } from 'tsyringe';
+
+import IStorageProvider from './models/IStorageProvider';
+import DiskStorageProvider from './implementations/DiskStorageProvider';
+import S3StorageProvider from './implementations/S3StorageProvider';
+
+import uploadConfig from '@config/upload';
+
+const providers = {
+  disk: DiskStorageProvider,
+  s3: S3StorageProvider,
+};
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  providers[uploadConfig.driver],
+);
