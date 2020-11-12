e2da029ba317b52e3e1511027fd444e03435a613
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-11-12 20:25:17 -0300

    Test profile service

---
+++ src/modules/users/services/ShowProfileService.spec.ts
@@ -0,0 +1,34 @@
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import AppError from '@shared/errors/AppError';
+import ShowProfileService from './ShowProfileService';
+
+describe('UpdateProfileService', () => {
+  let fakeUsersRepository: FakeUsersRepository;
+  let showProfile: ShowProfileService;
+
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    showProfile = new ShowProfileService(fakeUsersRepository);
+  });
+
+  it('should be able to show the profile', async () => {
+    const name = 'John Doe';
+    const email = 'johndoe@gmail.com';
+    const user = await fakeUsersRepository.create({
+      name,
+      email,
+      password: '79345794',
+    });
+
+    const profile = await showProfile.execute({ user_id: user.id });
+
+    expect(profile.name).toBe(name);
+    expect(profile.email).toBe(email);
+  });
+
+  it('should not be able to show the profile from non existing user', async () => {
+    await expect(
+      showProfile.execute({ user_id: '3874548' }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
