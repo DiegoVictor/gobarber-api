7c4c1436a60f982ded41b79e2d8b9a87384df008
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-11-12 20:21:28 -0300

    Test user creation

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,51 @@
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import CreateUserService from './CreateUserService';
+import AppError from '@shared/errors/AppError';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import FakeCacheProvider from '@shared/container/providers/CacheProvider/fakes/FakeCacheProvider';
+
+describe('CreateUserService', () => {
+  let fakeUsersRepository: FakeUsersRepository;
+  let fakeHashProvider: FakeHashProvider;
+  let fakeCacheProvider: FakeCacheProvider;
+  let createUser: CreateUserService;
+
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+    fakeCacheProvider = new FakeCacheProvider();
+
+    createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+      fakeCacheProvider,
+    );
+  });
+
+  it('should be able to create a new user', async () => {
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '79345794',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with an email already in use ', async () => {
+    const email = 'johndoe@gmail.com';
+    await createUser.execute({
+      name: 'John Doe',
+      email,
+      password: '79345794',
+    });
+
+    await expect(
+      createUser.execute({
+        name: 'Jane Doe',
+        email,
+        password: '8927349',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
