764cf0949f1ce0368798eed945a76bc62660c9a4
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-11-15 11:24:21 -0300

    Configure profile routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -5,6 +5,7 @@ import providersRoutes from '@modules/appointments/infra/http/routes/providers.r
 import usersRoutes from '@modules/users/infra/http/routes/users.routes';
 import sessionsRoutes from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRoutes from '@modules/users/infra/http/routes/password.routes';
+import profileRoutes from '@modules/users/infra/http/routes/profile.routes';
 
 const routes = Router();
 
@@ -13,5 +14,8 @@ routes.use('/sessions', sessionsRoutes);
 routes.use('/providers', providersRoutes);
 routes.use('/appointments', appointmentsRoutes);
 routes.use('/users', usersRoutes);
+
 routes.use('/password', passwordRoutes);
+routes.use('/profile', profileRoutes);
+
 export default routes;
