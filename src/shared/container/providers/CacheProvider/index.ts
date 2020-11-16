20b80420524def7f2659987704afd3ee98bf6598
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-11-16 20:56:35 -0300

    Use fake provider

--- src/shared/container/providers/CacheProvider/index.ts
+++ src/shared/container/providers/CacheProvider/index.ts
@@ -2,9 +2,11 @@ import { container } from 'tsyringe';
 
 import ICacheProvider from './models/ICacheProvider';
 import RedisCacheProvider from './implementations/RedisCacheProvider';
+import FakeCacheProvider from './fakes/FakeCacheProvider';
 
 const providers = {
-  redis: RedisCacheProvider,
+  redis:
+    process.env.NODE_ENV !== 'test' ? RedisCacheProvider : FakeCacheProvider,
 };
 
 container.registerSingleton<ICacheProvider>('CacheProvider', providers.redis);
