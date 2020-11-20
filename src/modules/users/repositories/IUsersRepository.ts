1f48ac584f9b68cc542afb79a6aea3f1599826d3
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-11-20 20:08:51 -0300

    Create count method

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -4,6 +4,7 @@ import IFindAllProvidersDTO from '../dtos/IFindAllProvidersDTO';
 
 export default interface IUsersRepository {
   findAllProviders(data: IFindAllProvidersDTO): Promise<User[]>;
+  countProviders(except_user_id?: string): Promise<number>;
   findById(id: string): Promise<User | undefined>;
   findByEmail(email: string): Promise<User | undefined>;
   create(data: ICreateUserDTO): Promise<User>;
