efc0fabc38d34be6826d25584a1d681aaf5d49b6
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-11-19 21:02:53 -0300

    Test with data from cache

--- tests/unit/ListProviderAppointmentsService.spec.ts
+++ tests/unit/ListProviderAppointmentsService.spec.ts
@@ -42,4 +42,38 @@ describe('ListProviderAppointmentsService', () => {
 
     expect(appointments).toEqual([appointment1, appointment2]);
   });
+
+  it('should be able to list the appointments on a specific day from cache', async () => {
+    const provider_id = String(faker.random.number());
+    const user_id = String(faker.random.number());
+
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id,
+      user_id,
+      date: new Date(2020, 6, 20, 14, 0, 0),
+    });
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id,
+      user_id,
+      date: new Date(2020, 6, 20, 15, 0, 0),
+    });
+
+    await fakeCacheProvider.save(
+      `provider_appointment:${provider_id}:2020-7-20`,
+      [appointment1, appointment2],
+    );
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id,
+      day: 20,
+      year: 2020,
+      month: 7,
+    });
+
+    appointments.forEach(appointment => {
+      expect(appointments).toContainEqual({
+        ...appointment,
+      });
+    });
+  });
 });
