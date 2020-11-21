53b12de18373cdb39062b129f70019300b202a77
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-11-21 13:04:38 -0300

    Return count and paginated content

--- src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -3,15 +3,41 @@ import { container } from 'tsyringe';
 import { classToClass } from 'class-transformer';
 
 import ListProviderService from '@modules/appointments/services/ListProviderService';
+import CountProvidersService from '@modules/appointments/services/CountProvidersService';
+import PaginationLinks from '@shared/helpers/PaginationLinks';
+
+interface CustomRequest {
+  query: {
+    page: number;
+  };
+}
 
 export default class ProvidersController {
-  public async index(request: Request, response: Response): Promise<Response> {
+  public async index(
+    request: Request & CustomRequest,
+    response: Response,
+  ): Promise<Response> {
+    const { currentUrl } = request;
+    let { page = 1 } = request.query;
     const user_id = request.user.id;
+    const take = 30;
 
     const listProvider = container.resolve(ListProviderService);
     const providers = await listProvider.execute({
       user_id,
+      page,
+      take,
     });
+
+    const countProviders = container.resolve(CountProvidersService);
+    const count = await countProviders.execute(user_id);
+
+    response.header('X-Total-Count', String(count));
+
+    const pagesTotal = Math.ceil(count / take);
+    if (pagesTotal > 1) {
+      response.links(PaginationLinks(page, pagesTotal, currentUrl));
+    }
 
     return response.json(classToClass(providers));
   }
