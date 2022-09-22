5580992ddddd774a49d08d58f1c5e829cfccf0f6
Diego Victor <diegovictorgonzaga@gmail.com>
2022-09-21 23:23:46 -0300

    Fix error message

--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -34,7 +34,7 @@ class ResetPasswordService {
     const user = await this.usersRepository.findById(userToken.user_id);
 
     if (!user) {
-      throw new AppError('user does not exists');
+      throw new AppError('User does not exists');
     }
 
     const tokenCreatedAt = userToken.created_at;
